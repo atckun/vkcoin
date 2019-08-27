c32d71f426c535ee7f792924c2242c8cf5183526
Deeplerg <47917846+Deeplerg@users.noreply.github.com>
2019-08-27 20:46:07 +0500

    RateLimiter, VkCoinException

---
+++ VkCoin/Exceptions/VkCoinException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VkCoin.Exceptions
+{
+
+    [Serializable]
+    public class VkCoinException : Exception
+    {
+        private const string defaultMessage = "VK Coin API вернул ошибку";
+        private const string additional = "VK Coin API вернул ошибку:\n";
+
+        public VkCoinException() : base(defaultMessage) { }
+        public VkCoinException(string message) : base(additional + message) { }
+        public VkCoinException(string message, Exception inner) : base(message, inner) { }
+        protected VkCoinException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
